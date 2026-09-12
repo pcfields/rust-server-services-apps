7ed97b2e410f9740949cb78ff9f3fab3d82e299d
Peter Fields <pcfields@gmail.com>
2026-09-12 22:37:28 +0200

    feat(tcpserver): accept connections on port 3000

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+
+    println!("Running on port 3000");
+
+    for stream in connection_listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established");
+    }
 }
